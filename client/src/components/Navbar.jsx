441e6695a7f5308ec42f3edb42efa8f5d923af4e
beni diasonama <diasonamabeni10@gmail.com>
2025-07-25 10:02:45 -0400

    fix navbar link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,16 @@
 import { NavLink } from "react-router-dom";
 import React from 'react';
+import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Navbar() {
     const isAuthenticated = () => {
-    return !!localStorage.getItem('token');
+        return !!localStorage.getItem('token');
     };
   return (
     <div>
       <nav className="flex justify-between items-center mb-6">
-        <NavLink to="/">
+        <NavLink to="/admin">
           <img alt="Rocket logo" style={{"width" : 12 + '%'}} src="../src/assets/images/rocketLogo.png"></img>
         </NavLink>
 
